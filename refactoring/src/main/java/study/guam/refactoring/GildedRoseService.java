c67345cb9d3bf1b75ea5edc2807738557c63da9d
wq <kyu21@outlook.com>
2021-02-06 23:43:43 +0900

    Revert GildedRose code to original

--- refactoring/src/main/java/study/guam/refactoring/GildedRoseService.java
+++ refactoring/src/main/java/study/guam/refactoring/GildedRoseService.java
@@ -2,8 +2,6 @@ package study.guam.refactoring;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import study.guam.refactoring.domain.Item;
-import study.guam.refactoring.domain.ItemRepository;
 
 import java.util.List;
 
@@ -11,57 +9,53 @@ import java.util.List;
 @RequiredArgsConstructor
 public class GildedRoseService {
 
-    private final ItemRepository itemRepository;
-
-    public void updateQuality() {
-        List<Item> items = itemRepository.findAll();
-
+    public void updateQuality(List<Item> items) {
         for (Item item : items) {
-            if (!item.getName().equals("Aged Brie")
-                    && !item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.getQuality() > 0) {
-                    if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
-                        item.setQuality(item.getQuality() - 1);
+            if (!item.name.equals("Aged Brie")
+                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality > 0) {
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        item.quality = item.quality - 1;
                     }
                 }
             } else {
-                if (item.getQuality() < 50) {
-                    item.setQuality(item.getQuality() + 1);
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                    if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.getSellIn() < 11) {
-                            if (item.getQuality() < 50) {
-                                item.setQuality(item.getQuality() + 1);
+                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
 
-                        if (item.getSellIn() < 6) {
-                            if (item.getQuality() < 50) {
-                                item.setQuality(item.getQuality() + 1);
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
-                item.setSellIn(item.getSellIn() - 1);
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (item.getSellIn() < 0) {
-                if (!item.getName().equals("Aged Brie")) {
-                    if (!item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.getQuality() > 0) {
-                            if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
-                                item.setQuality(item.getQuality() - 1);
+            if (item.sellIn < 0) {
+                if (!item.name.equals("Aged Brie")) {
+                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (item.quality > 0) {
+                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                                item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        item.setQuality(item.getQuality() - item.getQuality());
+                        item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (item.getQuality() < 50) {
-                        item.setQuality(item.getQuality() + 1);
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
--- refactoring/src/main/java/study/guam/refactoring/RunController.java
+++ refactoring/src/main/java/study/guam/refactoring/RunController.java
@@ -2,7 +2,9 @@ package study.guam.refactoring;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -10,7 +12,7 @@ public class RunController {
     private final GildedRoseService gildedRoseService;
 
     @GetMapping("/run")
-    public void run() {
-        gildedRoseService.updateQuality();
+    public void run(@RequestBody List<Item> items) {
+        gildedRoseService.updateQuality(items);
     }
 }
